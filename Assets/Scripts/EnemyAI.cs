52d75b688411b833eaad7c8e60c122953fd66103
Dr. Arun Kulshreshth <arunkul@louisiana.edu>
2022-11-14 14:17:48 -0600

    Class Changes on 11/14/2022
    Added Enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// FSM States for the enemy
+public enum EnemyState { ATTACK, CHASE, MOVING, DEFAULT };
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+    public float chaseDistance = 20.0f;
+
+    protected EnemyState state = EnemyState.DEFAULT;
+    protected Vector3 destination = Vector3.zero;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = this.GetComponent<NavMeshAgent>();
+        animator= GetComponent<Animator>();
+    }
+
+    private Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleEnemyAIStates();
+    }
+
+    void HandleEnemyAIStates()
+    {
+        //Debug.Log("Enemy State is " + state);
+        switch (state)
+        {
+            case EnemyState.DEFAULT:
+                destination = transform.position + RandomPosition();
+                animator.SetBool("isWalking", false);
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
+                {
+                    state = EnemyState.CHASE;
+                }
+                else
+                {
+                    state = EnemyState.MOVING;
+                    agent.SetDestination(destination);
+                }
+                break;
+            case EnemyState.MOVING:
+                //Debug.Log("Dest = " + destination);
+                animator.SetBool("isWalking", true);
+                if (Vector3.Distance(transform.position, destination) < 0.05f)
+                {
+                    state = EnemyState.DEFAULT;
+                }
+
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
+                {
+                    state = EnemyState.CHASE;
+                }
+                break;
+            case EnemyState.CHASE:
+                if (Vector3.Distance(transform.position, player.transform.position) > chaseDistance)
+                {
+                    state = EnemyState.DEFAULT;
+                }
+                agent.SetDestination(player.transform.position);
+                animator.SetBool("isWalking", true);
+                agent.speed = 3.0f;
+                if (Vector3.Distance(transform.position, player.transform.position) <= agent.stoppingDistance)
+                {
+                    state = EnemyState.ATTACK;
+                }
+                break;
+            case EnemyState.ATTACK:
+                animator.SetBool("isAttacking", true);
+                animator.SetBool("isWalking", false);
+                if (Vector3.Distance(transform.position, player.transform.position) > agent.stoppingDistance)
+                {
+                    state = EnemyState.CHASE;
+                    animator.SetBool("isAttacking", false);
+                }
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
